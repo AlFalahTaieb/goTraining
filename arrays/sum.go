2a02d2f6aee9bf286b2ba1fa8f68f9a1f30e4fb3
melfelah <melfelah@dxc.com>
2022-10-29 06:24:31 +0200

    update sum

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,8 +2,8 @@ package main
 
 func sum(numbers []int) int {
 	res := 0
-	for i := 0; i < len(numbers); i++ {
-		res += numbers[i]
+	for _, number := range numbers {
+		res += number
 	}
 	return res
 }
